d19058e5910850f8408c8c677ebcaebaf47d5b9c
Marcelo Gornstein <marcelog@gmail.com>
2014-06-16 20:37:39 -0300

    removing start phases

--- src/egetter_app.erl
+++ src/egetter_app.erl
@@ -6,20 +6,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Exports.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--export([start_phase/3, start/2, stop/1]).
+-export([start/2, stop/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Public API.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--spec start_phase(
-  atom(), normal|{takeover, node()}|{failover, node()}, term()
-) -> ok.
-start_phase(read_user_agents, _Type, _PhaseArgs) ->
-  ok = load(user_agents);
-
-start_phase(read_proxies, _Type, _PhaseArgs) ->
-  ok = load(proxies).
-
 -spec start(atom(), term()) -> {ok, pid()}.
 start(_StartType, _StartArgs) ->
   egetter_sup:start_link().
@@ -28,26 +19,4 @@ start(_StartType, _StartArgs) ->
 stop(_State) ->
   ok.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Private API.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--spec load(user_agents|proxies) -> ok.
-load(Type) ->
-  Ets = new_ets(),
-  case egetter:cfg_get(Type) of
-    undefined -> lager:debug("NOT READING ~p", [Type]);
-    File ->
-      lager:debug("Reading ~p from ~p", [Type, File]),
-      ok = egetter_file:load(Ets, File)
-  end,
-  OptName = list_to_atom(lists:concat([Type, "_ets"])),
-  _ = egetter:cfg_set(OptName, Ets),
-  ok.
 
--spec new_ets() -> ets:tid().
-new_ets() ->
-  ets:new(ets_table, [
-    public, ordered_set,
-    {write_concurrency, false}, {read_concurrency, true},
-    compressed
-  ]).
--- src/egetter_sup.erl
+++ src/egetter_sup.erl
@@ -26,9 +26,34 @@ init([]) ->
   egetter = ets:new(egetter:internal_ets(), [
     named_table, public, {write_concurrency, false}, {read_concurrency, true}
   ]),
+  ok = load(user_agents),
+  ok = load(proxies),
   Children = [
     ?SUP(ibrowse_sup),
     ?SUP(ssl_sup)
   ],
   {ok, {{one_for_one, 5, 10}, Children}}.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Private API.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-spec load(user_agents|proxies) -> ok.
+load(Type) ->
+  Ets = new_ets(),
+  case egetter:cfg_get(Type) of
+    undefined -> lager:debug("NOT READING ~p", [Type]);
+    File ->
+      lager:debug("Reading ~p from ~p", [Type, File]),
+      ok = egetter_file:load(Ets, File)
+  end,
+  OptName = list_to_atom(lists:concat([Type, "_ets"])),
+  _ = egetter:cfg_set(OptName, Ets),
+  ok.
+
+-spec new_ets() -> ets:tid().
+new_ets() ->
+  ets:new(ets_table, [
+    public, ordered_set,
+    {write_concurrency, false}, {read_concurrency, true},
+    compressed
+  ]).
